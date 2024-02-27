e4c874b1f6a9eb5a8fbd591d7e1ecf93ca204b1e
Timmi1015 <aniskovichav@gmail.com>
2024-02-27 18:03:28 +0300

    add MatrixWorker.findMaxAboveDiagonal

--- src/by/itstep/aniskovich/java/stage15/model/MatrixWorker.java
+++ src/by/itstep/aniskovich/java/stage15/model/MatrixWorker.java
@@ -1,13 +1,30 @@
 package by.itstep.aniskovich.java.stage15.model;
 
 public class MatrixWorker {
+    public static int findMaxAboveDiagonal(int[][] matrix) {
+        // check proof matrix
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
 
 
-    private static int sumDiagonalMatrix(int[][] matrix) {
+    private static int sumIndexOverDiagonalMatrix(int[][] matrix) {
         int sum = 0;
 
         for (int i = 0; i < matrix[i].length; i++) {
-            sum += matrix[i][i];
+            for (int j = i - 1; j < matrix[i].length ; j++) {
+                sum += matrix[i][i];
+            }
+
         }
         return sum;
     }
@@ -17,6 +34,22 @@ public class MatrixWorker {
 
         for (int i = 0; i < matrix[i].length - 1; i++) {
             sum += matrix[i][matrix.length - 1 - i];
+        }
+        return sum;
+    }
+
+    private static int sumRowMatrix(int[][] matrix, int i) {
+        int sum = 0;
+        for (int j = 0; j < matrix[i].length - 1; j++) {
+            sum += matrix[i][j];
+        }
+        return sum;
+    }
+
+    private static int sumColumnMatrix(int[][] matrix, int j) {
+        int sum = 0;
+        for (int i = 0; i < matrix[i].length - 1; i++) {
+            sum += matrix[i][j];
         }
         return sum;
     }
