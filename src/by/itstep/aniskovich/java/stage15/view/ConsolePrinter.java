e90c158286ef65fac4ef5fbe3ffdf9e32e90c9e1
Timmi1015 <aniskovichav@gmail.com>
2024-02-28 13:10:18 +0300

    add ConsolePrinter.printMatrix

--- src/by/itstep/aniskovich/java/stage15/view/ConsolePrinter.java
+++ src/by/itstep/aniskovich/java/stage15/view/ConsolePrinter.java
@@ -4,4 +4,16 @@ public class ConsolePrinter {
     public static void print(String msg) {
         System.out.print(msg);
     }
+
+    public static String printMatrix(int[][] matrix) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                builder.append(matrix[i][j]).append("\t");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
 }
